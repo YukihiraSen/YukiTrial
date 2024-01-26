6047091e28d192c2c42ee2ef49e23e495e9646f3
YukihiraSen <156506588+YukihiraSen@users.noreply.github.com>
2024-01-26 22:23:24 +0800

    Update yuki.py

--- yuki.py
+++ yuki.py
@@ -5,39 +5,6 @@ time.sleep(1.5)
 print('[\033[1;32m✓\033[1;37m] Wait For Update Tool !! ')
 time.sleep(1.5)
 os.system('git pull')
-os.system('clear')
-def back():
-	login()
-
-ah="TRICKER-"
-imt="-M4786=="
-ak=" ALIYAN-"
-myid=uuid.uuid4().hex[:10].upper()
-try:
-	key1 = open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
-except:
-	kok=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'w')
-	kok.write(myid+imt)
-	kok.close()
-def login():
-	try:
-		token = open('.token.txt','r').read()
-		tokenku.append(token)
-		try:
-			sy = requests.get('https://graph.facebook.com/me?access_token='+tokenku[0])
-			public_menu()
-		except KeyError:
-			Public()
-		except requests.exceptions.ConnectionError:
-			clear()
-			print(logo)
-			print ( ' [×] Connection Timeout')
-			exit()
-	except IOError:
-		Public()
-def jalan(z):
-	for e in z + '\n':
-		sys.stdout.write(e);sys.stdout.flush();time.sleep(0.01)
 print('[\033[1;32m✓\033[1;37m] Update Done \033[1;32m✓\033[1;37m Now You Can Use This Tool :) ')
 time.sleep(2)
 os.system(f'xdg-open https://facebook.com/groups/1848481151967680/')
@@ -410,41 +377,4 @@ def m4(ids,names,passlist):
                 time.sleep(20)
         loop+=1
         
-def Fof():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
-	clear()
-	print(logo)
-	r1=requests.get("https://github.com/YukihiraSen/Yuki/blob/main/approval.txt").text
-	if key1 in r1:
-		os.system('clear')
-		print(logo)
-		Main()
-	else:
-		os.system("clear")
-		print(logo)
-		print("\t \033[1;32m First Get Approvel\033[1;37m ")
-		time.sleep(1)
-		os.system("clear")
-		print(logo)
-		print ("")
-		print(" \033[1;32m Free BUT You Need Get Approved First\033[1;37m\n")
-		print(" \033[1;32m Note : ENJOY   \033[1;37m")
-		print ("")
-		print(" Your Key is Not Approved ")
-		print("")
-		print(" Copy And Send Key To Admin")
-		print ("")
-		print (" Your Key : "+ak+ah+key1 )
-		print ("")
-		name = input(" Your Name : ")
-		print ("")
-		gf = input(" Your gf Name : ")
-		print ("")
-		lol = input(" Your Your Email : ")
-		print ("")
-		input(" Press Enter To Send Key")
-		time.sleep(3.5)
-		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Email%20:%20'+lol+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ah+key1
-		os.system('am start https://wa.me/+9635303304?text=' + tks)
-		Fof()        
-Fof()
+(Fof)
