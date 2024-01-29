db08099406afcd1252a913661cdbcad48ed4fea0
YukihiraSen <156506588+YukihiraSen@users.noreply.github.com>
2024-01-29 15:39:42 +0800

    Update yuki.py

--- yuki.py
+++ yuki.py
@@ -16,9 +16,9 @@ imt="-brand=="
 ak="-pro"
 myid=uuid.uuid4().hex[:10].upper()
 try:
-        key1 = open('/data/data/com.termux/files/usr/bin/.MRD-cov', 'r').read()
+        key1 = open(/data/data/com.termux/files/home/YukiTrial/yuki.py).read()
 except:
-        kok=open('/data/data/com.termux/files/usr/bin/.MRD-cov', 'w')
+        kok=open('/data/data/com.termux/files/home/YukiTrial/yuki.py')
         kok.write(myid+imt)
         kok.close()
 def login():
