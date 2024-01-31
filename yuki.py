73698a991e1e86778573721510a9dcbc0ddb6c81
YukihiraSen <156506588+YukihiraSen@users.noreply.github.com>
2024-01-31 11:56:31 +0800

    Update yuki.py

--- yuki.py
+++ yuki.py
@@ -29,7 +29,7 @@ import platform
 import base64
 import subprocess
 import os,base64,zlib,pip,urllib,time
-runtxt('[\033[1;32m✓\033[1;37m] Checking For Update !! ')
+runtxt("\n'[\033[1;32m✓\033[1;37m] Checking For Update !! '")
 time.sleep(1.5)
 runtxt('[\033[1;32m✓\033[1;37m] Wait For Update Tool !! ')
 time.sleep(1.5)
